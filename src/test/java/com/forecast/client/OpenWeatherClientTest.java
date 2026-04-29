583d8f1e463086605d9089ddaf11e050dbeb21b7
Matsvei Prakapovich <matveyprokopovech@gmail.com>
2026-04-29 23:06:17 +0300

    test: add test for empty list

--- src/test/java/com/forecast/client/OpenWeatherClientTest.java
+++ src/test/java/com/forecast/client/OpenWeatherClientTest.java
@@ -169,4 +169,26 @@ class OpenWeatherClientTest {
 
         mockServer.verify();
     }
+
+    @Test
+    void getForecast_EmptyList() {
+        String jsonResponse = """
+                {
+                  "list": [
+                  ]
+                }
+                """;
+
+        mockServer.expect(requestTo("http://mock-openweather.com/forecast?appid=test-api-key&" +
+                        "lat=53.9006&lon=27.5590&units=metric"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(jsonResponse, MediaType.APPLICATION_JSON));
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () ->
+                client.getForecast(new BigDecimal("53.9006"), new BigDecimal("27.5590")));
+
+        assertEquals("failed to decode response: missing forecast list", exception.getMessage());
+
+        mockServer.verify();
+    }
 }
